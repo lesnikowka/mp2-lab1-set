17e7e2a3405fcb8d7a26d4a5d6e5489a6fb04867
lesnikov nikita <lesnikovnikita65@gmail.com>
2022-09-16 15:04:16 +0300

    fixed bug constructor

--- src/tbitfield.cpp
+++ src/tbitfield.cpp
@@ -17,6 +17,9 @@ TBitField::TBitField(int len)
         MemLen = len / (sizeof(TELEM) * 8) + 1;
 
         pMem = new TELEM[MemLen];
+
+        for (int i = 0; i < MemLen; i++)
+            pMem[i] = 0;
     }
 }
 
